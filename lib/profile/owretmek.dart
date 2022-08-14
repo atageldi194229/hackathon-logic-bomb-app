054a9268029e954401fc9106e0da3b39ca92ec96
atageldi194229 <atageldi194229@gmail.com>
2022-08-15 01:25:31 +0500

    ci

--- lib/profile/owretmek.dart
+++ lib/profile/owretmek.dart
@@ -16,24 +16,31 @@ class _OwretmekState extends State<Owretmek> {
       appBar: AppBar(
         toolbarHeight: 40,
         elevation: 0,
-        backgroundColor:Colors.white,
+        backgroundColor: Colors.white,
         actions: [
           Padding(
-            padding: const EdgeInsets.only(top: 12,right: 34),
-            child: Icon(Icons.settings,color: Colors.black,),
+            padding: const EdgeInsets.only(top: 12, right: 34),
+            child: Icon(
+              Icons.settings,
+              color: Colors.black,
+            ),
           ),
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.only(left: 18,right: 38),
+        padding: const EdgeInsets.only(left: 18, right: 38),
         child: ListView(
           children: [
             Padding(
               padding: const EdgeInsets.only(bottom: 10),
-              child: CircleAvatar(radius: 50,
+              child: CircleAvatar(
+                radius: 50,
                 child: ClipRRect(
-                  child: CachedNetworkImage(imageUrl: 'https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=600',
-                  fit: BoxFit.cover,),
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        'https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=600',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
@@ -41,10 +48,18 @@ class _OwretmekState extends State<Owretmek> {
               keyboardType: TextInputType.name,
               decoration: InputDecoration(
                 hintText: "At",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(width: 1,color: Color.fromRGBO(185, 185, 185, 1),),),
-                hintStyle: TextStyle(fontWeight: FontWeight.w400,fontSize:16,
-                  color: Color.fromRGBO(185, 185, 185, 1),),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Color.fromRGBO(185, 185, 185, 1),
+                  ),
+                ),
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Color.fromRGBO(185, 185, 185, 1),
+                ),
               ),
             ),
             Padding(
@@ -55,22 +70,36 @@ class _OwretmekState extends State<Owretmek> {
                 decoration: InputDecoration(
                   hintText: "Reýting(141200)",
                   suffixIcon: Image.asset("../asset/img/star.png"),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(width: 1,color: Color.fromRGBO(185, 185, 185, 1),),),
-                  hintStyle: TextStyle(fontWeight: FontWeight.w400,fontSize:16,
-                    color: Color.fromRGBO(185, 185, 185, 1),),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color.fromRGBO(185, 185, 185, 1),
+                    ),
+                  ),
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: Color.fromRGBO(185, 185, 185, 1),
+                  ),
                 ),
               ),
             ),
-           CachedNetworkImage(imageUrl: imageUrl),
+            CachedNetworkImage(imageUrl: "imageUrl"),
             Container(
               alignment: Alignment.center,
-              height: 34,width: 127,
+              height: 34,
+              width: 127,
               margin: EdgeInsets.only(top: 70),
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-                  color: Colors.black),
-              child: Text("Sahypa acmak",style: TextStyle(fontWeight: FontWeight.w700,fontSize:12,
-                  color: Colors.white),),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.black),
+              child: Text(
+                "Sahypa acmak",
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 12,
+                    color: Colors.white),
+              ),
             )
           ],
         ),
